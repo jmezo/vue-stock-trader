4bf2aef1d9f6652a1b2ec3aa616efe195758269e
Janos Mezo <jmezo1992@gmail.com>
2020-03-15 10:21:42 +0100

    set up stock listings

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,11 +1,27 @@
 <template>
-  <h1>stocks - stocks component</h1>
+  <div>
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+  </div>
 </template>
 
 <script>
-export default {
+  import Stock from './Stock.vue'
 
-}
+  export default {
+    data() {
+      return {
+        stocks: [
+          {id: 1, name: 'BMW', price: 110 },
+          {id: 2, name: 'Google', price: 210 },
+          {id: 3, name: 'Apple', price: 150 },
+          {id: 4, name: 'Twitter', price: 60 },
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
 </script>
 
 <style>
